2c32ebce3b4a1b595ee8c5db3a961eaeea1ade28
yunitasangadji <sangadjiyunita@gmail.com>
2024-12-05 19:56:03 +0700

    Disable line endings conversion

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Http\Controllers\Api;
+//import model Post
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+//import resource PostResource
+use App\Http\Resources\PostResource;
+//import Http request
+use Illuminate\Http\Request;
+//import facade Validator
+
+use Illuminate\Support\Facades\Validator;
+class PostController extends Controller
+{
+        /**
+        * index
+        *
+        * @return void
+        */
+public function index()
+{
+    //get all posts
+    $posts = Post::latest()->paginate(5);
+    //return collection of posts as a resource
+    return new PostResource(true, 'List Data Posts', $posts);
+}
+/**
+* store
+*
+* @param mixed $request
+* @return void
+*/
+public function store(Request $request)
+{
+    //define validation rules
+    $validator = Validator::make($request->all(), [
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        'title' => 'required',
+        'content' => 'required',
+]);
+//check if validation fails
+if ($validator->fails()) {
+    return response()->json($validator->errors(), 422);
+}
+//upload image
+$image = $request->file('image');
+$image->storeAs('public/posts', $image->hashName());
+
+//create post
+$post = Post::create([
+        'image' => $image->hashName(),
+        'title' => $request->title,
+        'content' => $request->content,
+]);
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
+    }
+}
